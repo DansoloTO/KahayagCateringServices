d2fe52be5688474bf716076511f936f3c9870b7e
DansoloTO <148069798+DansoloTO@users.noreply.github.com>
2026-04-09 01:17:53 +0800

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/src/App.jsx/', // Ensure slashes are at BOTH ends and use quotes
+  base: '/KahayagCateringServices/', // Ensure slashes are at BOTH ends and use quotes
 })
